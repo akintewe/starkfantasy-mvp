aa300383163c18ae8bc37949f55002308867240b
Marco Araya Jiménez <marcojime23@gmail.com>
2025-03-04 13:32:02 -0600

    initialize project structure with Vite, React, and TypeScript; add Tailwind CSS and Dojo configuration

---
+++ client/src/starknet-provider.tsx
@@ -0,0 +1,39 @@
+import type { PropsWithChildren } from "react";
+import { mainnet } from "@starknet-react/chains";
+import {
+    type Connector,
+    jsonRpcProvider,
+    StarknetConfig,
+    voyager,
+} from "@starknet-react/core";
+import { dojoConfig } from "../dojoConfig";
+import {
+    predeployedAccounts,
+    type PredeployedAccountsConnector,
+} from "@dojoengine/predeployed-connector";
+
+let pa: PredeployedAccountsConnector[] = [];
+predeployedAccounts({
+    rpc: dojoConfig.rpcUrl as string,
+    id: "katana",
+    name: "Katana",
+}).then((p) => (pa = p));
+
+export default function StarknetProvider({ children }: PropsWithChildren) {
+    const provider = jsonRpcProvider({
+        rpc: () => ({ nodeUrl: dojoConfig.rpcUrl as string }),
+    });
+
+    return (
+        <StarknetConfig
+            chains={[mainnet]}
+            provider={provider}
+            connectors={pa as unknown as Connector[]}
+            explorer={voyager}
+            autoConnect
+        >
+            {/* @ts-ignore react version mismatch */}
+            {children}
+        </StarknetConfig>
+    );
+}
