0112a780ca1b7c95ade691b2a860bdef219eacc7
Dario Zamora Rojas <dariozamora0731@gmail.com>
2025-03-12 15:58:48 -0600

    feat: Migrate Premier league page

--- client/src/components/header/page.tsx
+++ client/src/components/header/page.tsx
@@ -2,12 +2,18 @@ import Logo from "./components/logo";
 import NavBar from "./components/navbar";
 import UserBlock from "./components/userBlock";
 
-export default function header () {
-    return (
-        <>
+interface HeaderProps {
+  className?: string;
+}
+
+export default function Header({ className = "" }: HeaderProps) {
+  return (
+    <header className={`fixed top-0 left-0 w-full p-4 z-50 ${className}`}>
+      <div>
         <Logo />
         <NavBar />
         <UserBlock />
-        </>
-    );
+      </div>
+    </header>
+  );
 }
---
+++ client/src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,41 @@
+'use client';
+// ICONS
+import { TbSoccerField } from "react-icons/tb";
+import { MdLeaderboard } from "react-icons/md";
+import { SiCoinmarketcap } from "react-icons/si";
+import { FaRankingStar } from "react-icons/fa6";
+import { FaHistory } from "react-icons/fa";
+import { IoIosSettings } from "react-icons/io";
+import { BsLayoutTextWindow } from "react-icons/bs";
+
+import SidebarTab from './components/SidebarTap';
+import SidebarHeader from './components/SidebarHeader';
+
+const tabs = [
+  { id: 1, title: 'My Team', image: <TbSoccerField />, href: '/dashboard' },
+  // { id: 2, title: 'Leaderboard', image: <MdLeaderboard />, href: '/transactions' },
+  { id: 2, title: 'Market', image: <SiCoinmarketcap />, href: '/market' },
+  // { id: 4, title: 'Results', image: <BsLayoutTextWindow />, href: '/results' },
+  { id: 3, title: 'Player Rankings', image: <FaRankingStar />, href: '/rankings' },
+  // { id: 6, title: 'History', image: <FaHistory />, href: '/history' },
+  // { id: 7, title: 'Settings', image: <IoIosSettings />, href: '/settings' },
+];
+
+interface SidebarProps {
+  currentImage: string;
+  className?: string;
+}
+
+export default function Sidebar({ currentImage, className = "" }: SidebarProps) {
+  return (
+<div className={` h-screen max-w-[58px] bg-slate-900 shadow-lg hover:max-w-[250px] overflow-hidden transition-all ${className}`}>
+
+      <SidebarHeader imageSrc={currentImage} />
+      <div className="flex flex-col mt-4 space-y-2">
+        {tabs.map((tab, index) => (
+          <SidebarTab key={tab.id} {...tab} isLast={false} />
+        ))}
+      </div>
+    </div>
+  );
+}
