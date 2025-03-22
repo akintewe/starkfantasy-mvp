f60f8680bc1f0e3237c6c42287b23581cb0927bb
martin machiebe <martinmachiebe21@gmail.com>
2025-03-22 19:26:47 +0100

    feat: Create Performance Stats component

---
+++ client/src/app/leagues/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PerformanceStats from "./components/performanceStats";
+
+const page = () => {
+  return (
+    <div className="">
+      <PerformanceStats />
+    </div>
+  );
+};
+
+export default page;
